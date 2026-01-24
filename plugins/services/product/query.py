0932d4353bb53c6f146aee3c49289cbca6819537
Vigneshwaran-k <vigneshwaran.k.032003@gmail.com>
2026-01-24 11:44:43 +0530

    d3e and machine fields added

--- plugins/services/product/query.py
+++ plugins/services/product/query.py
@@ -1,6 +1,6 @@
 from sqlalchemy import select, func, cast, String
 from sqlalchemy.orm import aliased
-from models.products import Product, d3e
+from models.products import Product, D3E, ProductMachine, Environment, EnvironmentProduct
 from models.translation import Translate
 
 def product_base_query(limit: int = None, offset: int = None):
@@ -54,13 +54,43 @@ def product_base_query(limit: int = None, offset: int = None):
         # generated_url -> url (Assuming it contains a JSON string or similar that needs parsing)
         Product.swapUrl.label('url'),
 
-        # d3e
-        '''cast(func.json_object(
-            'id', d3e.id,
-            'company_id', d3e.id_societe,
-            'name', d3e.nom,
-            'price', d3e.prix
-        ), String).label('d3e'),'''
+        #d3e
+        cast(
+        func.json_object(
+        "id", D3E.id,
+        "company_id", D3E.id_societe,
+        "name", D3E.nom,
+        "price", D3E.prix
+        ), String
+        ).label("d3e"),
+
+        #Machine
+        cast(
+        func.json_object(
+        "id", ProductMachine.id,
+        "supplier_id", ProductMachine.id_fournisseur,
+        "supplier_reference", ProductMachine.ref_usine,
+        "repair_score", ProductMachine.repair_score,
+        "buying_price", ProductMachine.prix_achat,
+        "pricing_discount_a", ProductMachine.tarif_remise_a,
+        "pricing_discount_a_a", ProductMachine.tarif_remise_aa,
+        "pricing_discount_b", ProductMachine.tarif_remise_b,
+        "pricing_discount_c", ProductMachine.tarif_remise_c,
+        "permanent_p_p_i", ProductMachine.PPI_permanent,
+        "observed_public_price", ProductMachine.PPI_promo,
+        "cegid_d_p_r", ProductMachine.dpr_cegid,
+        "real_d_p_r", ProductMachine.dpr_reel,
+        "is_motor", ProductMachine.is_motor,
+        "is_repair_forbidden", ProductMachine.is_repair_forbidden,
+        "return_rate", ProductMachine.return_rate,
+        "max_return_rate", ProductMachine.max_return_rate,
+        "exploded_view", ProductMachine.vue_eclatee,
+        "universal_branch", ProductMachine.id_arborescence
+        ), String
+        ).label("machine"),
+
+
+
 
     ).select_from(Product)
 
@@ -71,7 +101,8 @@ def product_base_query(limit: int = None, offset: int = None):
     stmt = stmt.outerjoin(t_meta_title, Product.generated_meta_title == t_meta_title.id)
     stmt = stmt.outerjoin(t_meta_desc, Product.generated_meta_description == t_meta_desc.id)
     stmt = stmt.outerjoin(t_orig_refs, Product.generated_original_references == t_orig_refs.id)
-    #stmt = stmt.outerjoin(d3e, Product.id_d3e == d3e.id)
+    stmt = stmt.outerjoin(D3E, Product.id_d3e == D3E.id)
+    stmt = stmt.outerjoin(ProductMachine, Product.id_machine == ProductMachine.id)
 
     if limit is not None:
         stmt = stmt.limit(limit)
